9be4ba46f77c550c02247ca32a070e29b9baf8fb
fallenhamster <khaigenequah100@gmail.com>
2022-11-17 23:03:56 +0800

    Created header

--- app.py
+++ app.py
@@ -10,6 +10,13 @@ def home():
 def login():
     return render_template('login.html')
 
+@app.route('/header')
+def header():
+    return render_template('header.html')
+
+@app.route('/footer')
+def footer():
+    return render_template('footer.html')
 
 
 
