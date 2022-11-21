cb566e2686d697bfbdb2fdbe9a255ebcc3e35bd9
fallenhamster <khaigenequah100@gmail.com>
2022-11-22 01:13:51 +0800

    Can registered user

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-import sqlite3
-from flask import Flask, render_template, redirect, url_for
+import sqlite3 as sql
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
+    conn = sql.connect('database.db')
+    conn.row_factory = sql.Row
     return conn
 
 @app.route('/')
@@ -32,6 +32,27 @@ def base():
 def register():
     return render_template('register.html')
 
+@app.route('/addrec',methods=['POST'])
+def addrec():
+    if request.method == 'POST':
+        try:
+            first_name = request.form['first_name']
+            last_name = request.form['last_name']
+            email = request.form['email']
+            password = request.form['password']
+
+            with sql.connect("database.db") as conn:
+                cur = conn.cursor()
+                cur.execute("INSERT INTO User (first_name, last_name, email, password, type) VALUES (?,?,?,?,'customer')",(first_name,last_name,email,password))
+
+                conn.commit()
+                msg = "You have registered an account successfully"
+        except:
+                conn.rollback()
+                msg = "Register failed"
+        finally:
+                return render_template("home.html",msg=msg)
+                conn.close()
 
 
 
