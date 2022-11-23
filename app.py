e21e4bcceea86c473487d9bf0ac9b746e485a51d
fallenhamster <khaigenequah100@gmail.com>
2022-11-24 01:26:46 +0800

    Finally can login, not yet completed

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import sqlite3 as sql
 from flask import Flask, render_template, redirect, url_for, request, flash, session
 from wtforms import StringField, BooleanField, PasswordField, validators
 from flask_wtf import Form
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
+#login_manager = LoginManager()
+#login_manager.init_app(app)
 
 class UserForm(Form):
     first_name = StringField('first_name',[validators.DataRequired()])
@@ -13,6 +16,10 @@ class UserForm(Form):
     email = StringField('email',[validators.DataRequired()])
     password = PasswordField('password',[validators.Length(min = 6), validators.DataRequired(), validators.EqualTo('password_confirmation', message = 'Password Must Match')])
     password_confirmation = PasswordField('password_confirmation',[validators.DataRequired()])
+
+class LoginForm(Form):
+    email = StringField('email',[validators.DataRequired()])
+    password = PasswordField('password',[validators.DataRequired(),validators.Length(min = 6)])
 
 def get_db_connection():
     conn = sql.connect('database.db')
@@ -23,13 +30,30 @@ def get_db_connection():
 def home():
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods = ['GET','POST'])
 def login():
-    return render_template('login.html')
-
-@app.route('/base')
-def base():
-    return render_template('base.html')
+    form = LoginForm(request.form)
+    if  request.method == 'POST' and form.validate():
+        #hashed_pw = check_password_hash(form.password.data,"sha256")
+        with sql.connect("database.db") as conn:
+            cur = conn.cursor()
+            #cur.execute("SELECT * FROM User WHERE email = ? AND password = ?",(form.email.data, hashed_pw))
+            #user = cur.fetchall()
+            cur.execute("SELECT email FROM User WHERE email = ?",(form.email.data,))
+            email = cur.fetchall()
+            cur.execute("SELECT password FROM USER WHERE email = ?",(form.email.data,))
+            pwhash = cur.fetchone()
+            #if len(email) != 1 or not check_password_hash(pwhash[0],form.password.data):
+            if check_password_hash(pwhash[0],form.password.data):
+                return render_template('home.html')
+            #if user:
+                #session ['loggedin'] = True
+                #session ['id'] = user ['id']
+                #session ['username'] = user ['first_name'] + user ['last_name']
+                #message = "Login successfully"
+                #flash(message,'LoggedIn')
+                #return render_template('home.html')
+    return render_template('login.html',form = form)
 
 @app.route('/register',methods=['GET','POST'])
 def register():
@@ -46,6 +70,13 @@ def register():
         flash(message,'registered')
         return redirect(url_for('login'))
     return render_template('register.html', form = form)
+
+
+def logout():
+    session.pop('loggedin',None)
+    session.pop('id',None)
+    session.pop('username',None)
+    return redirect(url_for('/'))
 
 if __name__ == '__main__':
     app.run(debug = True)
